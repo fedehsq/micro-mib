4e23a52463e0ee34a9aa3fad422f7d9e8b9054d2
fedehsq <fedebernacca@gmail.com>
2021-11-20 12:07:08 +0100

    splitting users

---
+++ mib-api-gateway/mib/views/home.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template, redirect
+from flask_login import current_user
+
+home = Blueprint('home', __name__)
+
+
+@home.route('/', methods=['GET'])
+def index():
+    if current_user is not None and hasattr(current_user, 'id'):
+        # calculate the number of notifications 
+        # number of message received to read + number of message sent that have been read       
+
+        # get the list of messages that has to be read
+        """messages = db.session.query(Message).filter(Message.receiver == current_user.id).all()"""
+        return render_template("index.html", number = 0)
+    else:
+        return redirect("/login")
+
