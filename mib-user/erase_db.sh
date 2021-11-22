da0b287dcf689a682186afd9967dc32cf2e632ef
fedehsq <fedebernacca@gmail.com>
2021-11-22 09:56:20 +0100

    split

--- mib-user/erase_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm db.sqlite
-rm -rf migrations/
-flask db init
-flask db migrate
-flask db upgrade
--- mib-user/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# set the env to development
-export FLASK_ENV=development
-
-flask run
